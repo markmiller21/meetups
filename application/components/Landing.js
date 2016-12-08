18134bfe32f5c5475cd5fca36e04e3e5ac30a0a8
Mark Miller <mark.kilpatrick.miller@gmail.com>
2016-12-08 17:58:35 -0500

    flesh out homepage

--- application/components/Landing.js
+++ application/components/Landing.js
@@ -1,14 +1,18 @@
+import Icon from 'react-native-vector-icons/MaterialIcons';
 import React, { Component } from 'react';
 import {
-  View,
   Text,
-  TouchableOpacity
- } from 'react-native';
+  TouchableOpacity,
+  Image,
+  View
+} from 'react-native';
 
-import Icon from 'react-native-vector-icons/Ionicons';
-import NavigationBar from 'react-native-navbar';
 import Colors from '../styles/colors';
-import { globals } from '../styles';
+import { landingStyles, globals } from '../styles';
+
+const BackgroundImage = 'https://s3-us-west-2.amazonaws.com/assembliesapp/welcome%402x.png';
+const Logo = 'https://s3-us-west-2.amazonaws.com/assembliesapp/logo.png';
+const styles = landingStyles;
 
 class Landing extends Component{
   constructor(){
@@ -16,29 +20,41 @@ class Landing extends Component{
     this.visitDashboard = this.visitDashboard.bind(this);
   }
   visitDashboard(){
-    this.props.navigator.push({
-      name: 'Dashboard'
-    });
+    this.props.navigator.push({ name: 'Dashboard' })
   }
   render(){
-    let titleConfig = { title: 'Landing', tintColor: 'white' };
     return (
-      <View style={globals.flexContainer}>
-        <NavigationBar
-          title={titleConfig}
-          tintColor={Colors.brandPrimary}
-        />
-        <View style={globals.flexCenter}>
-          <Text style={globals.h2}>
-            This is the Landing Page
-          </Text>
-          <TouchableOpacity onPress={this.visitDashboard}>
-            <Text>Go to the Dashboard</Text>
-          </TouchableOpacity>
+      <View style={styles.container}>
+        <View style={styles.container}>
+          <Image
+            style={styles.backgroundImage}
+            source={{ uri: BackgroundImage }}
+          />
         </View>
+        <View style={globals.flexCenter}>
+          <Image
+            style={styles.logo}
+            source={{ uri: Logo }}
+          />
+          <Text style={[globals.lightText, globals.h2, globals.mb2]}>
+            assemblies
+          </Text>
+          <Text style={[globals.lightText, globals.h4]}>
+            Where Developers Connect
+          </Text>
+        </View>
+        <TouchableOpacity
+          style={globals.button}
+          onPress={this.visitDashboard}
+        >
+          <Icon name='ios-person' size={36} color='white' />
+          <Text style={globals.buttonText}>
+            {" "} Go to Dashboard
+          </Text>
+        </TouchableOpacity>
       </View>
-    )
+    );
   }
-}
+};
 
 export default Landing;
