5752c2db3344795e94704804291d40c3cc053ef7
Mark Miller <mark.kilpatrick.miller@gmail.com>
2016-12-08 17:38:05 -0500

    add clickable route -- to nothing

--- index.ios.js
+++ index.ios.js
@@ -5,11 +5,23 @@ import {
 } from 'react-native';
 
 import Landing from './application/components/Landing';
+import { globals } from './application/styles';
 
 class meetups extends Component {
   render() {
     return (
-      <Landing />
+      <Navigator
+        style={globals.flex}
+        initialRoute={{ name: 'Landing' }}
+        renderScene={(route, navigator) => {
+          switch(route.name){
+            case 'Landing':
+              return (
+                <Landing navigator={navigator}/>
+            );
+          }
+        }}
+      />
     );
   }
 }
